26e4617ce0145dfe3c19067bcf3434245346a59b
Maheez <maheezmtm@gmail.com>
2023-11-25 03:28:52 +0530

    index.js change

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import "slick-carousel/slick/slick.css";
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
