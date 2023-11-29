1a2e50db10bb44d8e58d5b2d31320c8f03267bec
Maheez <maheezmtm@gmail.com>
2023-11-29 23:54:31 +0530

    a

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -5,7 +5,7 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[450px] h-[450px] lgl:w-[900px] lgl:h-[900px] z-10"
+        className="w-[450px] h-[450px] lgl:w-[500px] lgl:h-[500px] z-10"
         src={bannerImg}
         alt="bannerImg"
       />
