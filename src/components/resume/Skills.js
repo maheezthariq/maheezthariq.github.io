ed23b0c6c8b198b9507cf1fe29e5309d73e26e47
Maheez <maheezmtm@gmail.com>
2023-12-08 01:18:45 +0530

    Update Skills.js

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -23,7 +23,7 @@ const Skills = () => {
                 initial={{ x: "-100%", opacity: 0 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{ duration: 0.5, delay: 0.5 }}
-                className="w-[80%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+                className="w-[70%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
               >
                 <span className="absolute -top-7 right-0">70%</span>
               </motion.span>
@@ -49,7 +49,7 @@ const Skills = () => {
                 initial={{ x: "-100%", opacity: 0 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{ duration: 0.5, delay: 0.5 }}
-                className="w-[60%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+                className="w-[80%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
               >
                 <span className="absolute -top-7 right-0">80%</span>
               </motion.span>
